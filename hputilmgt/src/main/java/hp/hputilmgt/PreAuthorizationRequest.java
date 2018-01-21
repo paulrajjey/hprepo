c96b5ab288d0ff7a0f788fe6fe4518c60fd916c9
bpmsAdmin <>
2018-01-20 19:59:12 -0600

    {/hputilmgt/src/main/java/hp/hputilmgt/PreAuthorizationRequest.java}

--- hputilmgt/src/main/java/hp/hputilmgt/PreAuthorizationRequest.java
+++ hputilmgt/src/main/java/hp/hputilmgt/PreAuthorizationRequest.java
@@ -9,9 +9,13 @@ public class PreAuthorizationRequest implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
+   @org.kie.api.definition.type.Label("Patient Name")
    private java.lang.String patientName;
+   @org.kie.api.definition.type.Label("age")
    private java.lang.Integer age;
+   @org.kie.api.definition.type.Label("Date Of Birth")
    private java.util.Date dob;
+   @org.kie.api.definition.type.Label("Patient Address")
    private java.lang.String patientAddress;
    private java.lang.String patientCity;
    private java.lang.String state;
